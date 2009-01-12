115c895cb6ecbbc222580ef069b2e73334be4f99
benramsey <benramsey@971a71fa-d690-11dd-a297-4fb579711a53>
2009-01-12 02:44:08 +0000

    Added support for a custom request/response object known to the Amazon library through interfaces.

--- trunk/library/Amazon/CloudFront/Distribution/List.php
+++ trunk/library/Amazon/CloudFront/Distribution/List.php
@@ -6,9 +6,10 @@
  *
  * This source file is subject to the Simplified BSD License that is bundled
  * with this package in the file LICENSE.
- * It is also available through the world-wide-web at this URL:
+ * It is also available through the World Wide Web at this URL:
  * http://awsphp.googlecode.com/svn/trunk/LICENSE
  *
+ * @category awsphp
  * @package Amazon
  * @subpackage CloudFront
  * @copyright Copyright (c) 2009, Ben Ramsey (benramsey.com)
@@ -22,6 +23,7 @@
 require_once 'Amazon/CloudFront/Distribution.php';
 
 /**
+ * @category awsphp
  * @package Amazon
  * @subpackage CloudFront
  * @copyright Copyright (c) 2009, Ben Ramsey (benramsey.com)
